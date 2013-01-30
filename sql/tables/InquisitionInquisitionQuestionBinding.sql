09b75d8af189f0333795d8fa9f2aa7615a770288
Isaac Grant <isaac@silverorange.com>
2013-01-30 19:37:51 +0000

    add an id column so that questions can appear more than once in an inquisition.
    svn commit r78270

--- sql/tables/InquisitionInquisitionQuestionBinding.sql
+++ sql/tables/InquisitionInquisitionQuestionBinding.sql
@@ -1,7 +1,8 @@
 create table InquisitionInquisitionQuestionBinding (
+	id serial,
 	inquisition integer not null references Inquisition(id) on delete cascade,
 	question integer not null references InquisitionQuestion(id) on delete cascade,
 	displayorder integer not null default 0,
 
-	primary key (inquisition, question)
+	primary key (id)
 );
