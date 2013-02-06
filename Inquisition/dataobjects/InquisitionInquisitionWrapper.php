088fb8327b9b7b6fb16eb32bc5d95fe037be0200
Charles Waddell <charles@silverorange.com>
2013-02-06 21:13:17 +0000

    This method is no longer needed since we are now using the binding object.
    svn commit r78607

--- Inquisition/dataobjects/InquisitionInquisitionWrapper.php
+++ Inquisition/dataobjects/InquisitionInquisitionWrapper.php
@@ -13,41 +13,6 @@ require_once 'Inquisition/dataobjects/InquisitionInquisition.php';
  */
 class InquisitionInquisitionWrapper extends SwatDBRecordsetWrapper
 {
-	// {{{ public function attachSubRecordset()
-
-	public function attachSubRecordset($name, $wrapper, $binding_field,
-		SwatDBRecordsetWrapper $recordset)
-	{
-		$this->checkDB();
-
-		// assign empty recordsets for all records in this set
-		foreach ($this as $record) {
-			$empty_recordset = new $wrapper();
-			$record->$name = $empty_recordset;
-		}
-
-		// split records into separate recordsets for records in this set
-		$current_record_id = null;
-		$current_recordset = null;
-		foreach ($recordset as $record) {
-			// Note: this is why we're subclassing this method.
-			// SwatDBRecordsetWrapper calls getInternalValue which returns null.
-			// There must be a better way to do this, but it works for now.
-			$record_id = $record->$binding_field;
-
-			if ($record_id !== $current_record_id) {
-				$current_record_id = $record_id;
-				$current_recordset = $this[$record_id]->$name;
-			}
-
-			$current_recordset->add($record);
-		}
-
-		return $recordset;
-	}
-
-	// }}}
-
 	// {{{ protected function init()
 
 	protected function init()
